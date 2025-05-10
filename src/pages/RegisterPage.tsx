df6cd262194897aa322fda6d950fe8952495769d
Iraduhaye Bukuru Paterne <116680462+IraduhayeBukuruPaterne1@users.noreply.github.com>
2025-05-10 15:47:08 +0000

    paterne

---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <h1>Register</h1>
+      {/* Add form here later */}
+    </div>
+  );
+};
+
+export default RegisterPage;
+
